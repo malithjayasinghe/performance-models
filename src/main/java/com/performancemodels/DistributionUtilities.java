d8a2ccfe78f19d9c1bca8bdb74dc14748376f93b
Malith Jayasinghe <malith.jayasinghe@gmail.com>
2017-01-26 14:58:55 +0530

    Add DistributionUtilities

---
+++ src/main/java/com/performancemodels/DistributionUtilities.java
@@ -0,0 +1,17 @@
+package com.performancemodels;
+
+/**
+ * Created by temp on 1/26/17.
+ */
+public class DistributionUtilities {
+
+    public static double getMoment(int i, String FileName){
+
+        return 0.0;
+    }
+
+
+}
+
+
+
