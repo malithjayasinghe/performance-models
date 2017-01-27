fe0cf8a3c58a142feabe51d6c532e5e23470f315
Thakshilapiu <thakshilapiumaliha@gmail.com>
2017-01-27 09:49:47 +0530

    Addin new method

--- src/main/java/com/performancemodels/DistributionUtilities.java
+++ src/main/java/com/performancemodels/DistributionUtilities.java
@@ -1,16 +1,51 @@
 package com.performancemodels;
 
+import java.io.*;
+
 /**
  * Created by temp on 1/26/17.
  */
 public class DistributionUtilities {
 
-    public static double getMoment(int i, String FileName){
+    public static double getMean(int i, String FileName){
 
-        return 0.0;
+        String text = null;
+        BufferedReader reader = null;
+        double sum=0;
+        double count=0;
+        double mean=0;
+        try {
+            reader = new BufferedReader(new FileReader(FileName));
+            while ((text = reader.readLine()) != null) {
+
+                sum=sum+Double.parseDouble(text);
+
+                count++;
+
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+        mean=sum/count;
+       System.out.println(""+sum+"   "+mean);
+
+
+        return mean;
+
+
+
     }
-
-
 }
 
 
