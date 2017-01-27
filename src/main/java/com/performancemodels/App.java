73a1f0ae67bc8680c6987dc7ca99c68d2e466c01
Thakshilapiu <thakshilapiumaliha@gmail.com>
2017-01-27 16:09:04 +0530

    Adding a new method to calculate Mean

--- src/main/java/com/performancemodels/App.java
+++ src/main/java/com/performancemodels/App.java
@@ -7,11 +7,9 @@ import java.io.*;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException {
-
-        DistributionUtilities c = new DistributionUtilities();
-        c.getMean(2, "con1_fixed_test2.jtl");
-
+    public static void main(String[] args){
+        Double d=DistributionUtilities.getMean(args[0]);
+        System.out.println("Mean = "+d);
 
     }
 }
--- src/main/java/com/performancemodels/DistributionUtilities.java
+++ src/main/java/com/performancemodels/DistributionUtilities.java
@@ -7,15 +7,13 @@ import java.io.*;
  */
 public class DistributionUtilities {
 
-    public static double getMean(int i, String FileName) {
+    public static double getMean(String FileName) {
 
         String sLineValue;
-        int moment = i;
         BufferedReader reader;
         double sum = 0;
         double count = 0;
         double mean;
-        if (moment==1) {
             try {
                 reader = new BufferedReader(new FileReader(FileName));
                 while ((sLineValue = reader.readLine()) != null) {
@@ -28,12 +26,8 @@ public class DistributionUtilities {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else {
-            System.out.println("vvvvvvvvvvv");
-        }
         mean = sum / count;
         return mean;
-
     }
 }
 
